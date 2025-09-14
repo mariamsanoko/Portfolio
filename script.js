1017b0a0cb929a55b64fa6b4040dcfd552399526
mariamsanoko <mariam.sanoko@gmail.com>
2025-09-14 17:04:30 +0200

    pages

---
+++ script.js
@@ -0,0 +1,13 @@
+// Menu mobile
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
+
+// Dark mode toggle
+const toggle = document.getElementById("dark-toggle");
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
